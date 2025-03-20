8dfd8891187b50b541e562c2412f013a54f58e99
Mayada-Elsayed-Mostafa <mayyadaelsayed8@gmail.com>
2025-03-20 06:20:12 +0200

    Add dependencies for core libraries, networking, image loading, and database

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.devtools.ksp")
+    id("kotlin-parcelize")
 }
 
 android {
@@ -56,4 +58,49 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+
+    // Scoped API for ViewModels with Compose
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose-android:2.8.7")
+
+    // LiveData & Compose Integration
+    val composeVersion = "1.0.0"
+    implementation("androidx.compose.runtime:runtime-livedata:$composeVersion")
+
+    // Coroutines Support
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
+
+    // Networking - Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // JSON Processing - Gson
+    implementation("com.google.code.gson:gson:2.10.1")
+
+    // Logging for Networking - OkHttp Interceptor
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.3")
+
+    // Image Loading - Glide
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
+
+    // Database - Room Persistence Library
+    implementation("androidx.room:room-ktx:2.6.1")
+    implementation("androidx.room:room-runtime:2.6.1")
+    ksp("androidx.room:room-compiler:2.6.1")
+
+    // Image Loading with Coil
+    implementation("io.coil-kt:coil-compose:2.0.0")
+
+    // Navigation - Jetpack Navigation for Compose
+    implementation("androidx.navigation:navigation-compose:2.8.8")
+
+    // Serialization for Navigation Arguments
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
+
+    // Custom Fonts for Compose UI
+    implementation("androidx.compose.ui:ui-text-google-fonts:1.4.0")
+
 }

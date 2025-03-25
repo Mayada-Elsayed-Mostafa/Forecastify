e51e9e37b923184d4be3d82afe8a0988ea679c1d
Mayada-Elsayed-Mostafa <mayyadaelsayed8@gmail.com>
2025-03-26 00:15:22 +0200

    Update HomeViewModel to use dynamic location for weather retrieval

--- app/src/main/java/com/example/forecastify/home/HomeViewModel.kt
+++ app/src/main/java/com/example/forecastify/home/HomeViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.forecastify.home
 
+import android.location.Location
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -10,23 +11,27 @@ import com.example.forecastify.data.repository.WeatherRepositoryImp
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
+const val REQUEST_LOCATION_CODE = 2002
+
 class HomeViewModel(private val repository: WeatherRepositoryImp) : ViewModel() {
 
-    var lat = 30.0444
-    var log = 31.2357
 
     private val mutableWeather: MutableLiveData<WeatherResponse> = MutableLiveData()
     val weather: LiveData<WeatherResponse> = mutableWeather
 
     private val mutableMessage: MutableLiveData<String> = MutableLiveData()
-    val message: LiveData<String> = mutableMessage
 
-    fun getWeather() {
+
+    fun getWeather(locationState: Location) {
         viewModelScope.launch(Dispatchers.IO) {
             try {
-                val result = repository.getWeather(true, lat = lat, lon = log)
+                val result = repository.getWeather(
+                    true,
+                    lat = locationState.latitude,
+                    lon = locationState.longitude
+                )
                 if (result != null) {
-                    var res: WeatherResponse = result
+                    val res: WeatherResponse = result
                     mutableWeather.postValue(res)
                 } else {
                     mutableMessage.postValue("Please try again later...")
@@ -39,6 +44,7 @@ class HomeViewModel(private val repository: WeatherRepositoryImp) : ViewModel() 
 
 }
 
+@Suppress("UNCHECKED_CAST")
 class HomeFactory(private val repository: WeatherRepositoryImp) : ViewModelProvider.Factory {
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {

6d8f9e3352a7bf47a6084ef75a420ce612ac5fdb
Mayada-Elsayed-Mostafa <mayyadaelsayed8@gmail.com>
2025-03-26 17:26:27 +0200

    Add methods for fetching and managing weather data.

--- app/src/main/java/com/example/forecastify/data/repository/WeatherRepository.kt
+++ app/src/main/java/com/example/forecastify/data/repository/WeatherRepository.kt
@@ -1,7 +1,17 @@
 package com.example.forecastify.data.repository
 
+import com.example.forecastify.data.models.ForecastResponse
+import com.example.forecastify.data.models.MainOfForecast
 import com.example.forecastify.data.models.WeatherResponse
 
 interface WeatherRepository {
+
+    suspend fun getWeather(isOnline: Boolean, lat: Double, lon: Double): WeatherResponse?
+
+    suspend fun getForecast(lat: Double, lon: Double): ForecastResponse?
+
+    suspend fun addWeather(weather: WeatherResponse): Long
+
+    suspend fun removeWeather(weather: WeatherResponse): Int
 
 }

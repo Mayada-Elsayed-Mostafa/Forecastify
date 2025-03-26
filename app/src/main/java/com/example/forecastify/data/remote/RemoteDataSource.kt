0662d97820cfcdcc2fd45dc777ad5132dbe5c7c5
Mayada-Elsayed-Mostafa <mayyadaelsayed8@gmail.com>
2025-03-26 17:34:46 +0200

    Add getForecastOfDay method for fetching weather forecasts.

--- app/src/main/java/com/example/forecastify/data/remote/RemoteDataSource.kt
+++ app/src/main/java/com/example/forecastify/data/remote/RemoteDataSource.kt
@@ -1,7 +1,11 @@
 package com.example.forecastify.data.remote
 
+import com.example.forecastify.data.models.ForecastResponse
 import com.example.forecastify.data.models.WeatherResponse
 
 interface RemoteDataSource {
+
     suspend fun getCurrentWeather(lat: Double, lon: Double): WeatherResponse?
+
+    suspend fun getForecastOfDay(lat: Double, lon: Double): ForecastResponse?
 }

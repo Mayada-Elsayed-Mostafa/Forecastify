db52f51c6ff7822d4a0036a56e0388b99ed11414
Mayada-Elsayed-Mostafa <mayyadaelsayed8@gmail.com>
2025-04-01 13:53:26 +0200

    Add LanguageConverter utility for language switching and number formatting
    - Implemented changeLanguage function to change the app's language based on a provided language code.
    - Added compatibility for both Android versions >= 13 and < 13 for setting the application's locale.
    - Introduced ormatNumber function to format integers according to the device's locale.
    - Used LocaleManager for Android 13 and AppCompatDelegate for earlier versions for locale management.

---
+++ app/src/main/java/com/example/forecastify/utils/LanguageConverter.kt
@@ -0,0 +1,28 @@
+package com.example.forecastify.utils
+
+import android.app.LocaleManager
+import android.content.Context
+import android.os.Build
+import android.os.LocaleList
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.os.LocaleListCompat
+import java.text.NumberFormat
+import java.util.Locale
+
+object LanguageConverter {
+    fun changeLanguage(  context: Context,languageCode: String) {
+        //version >= 13
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            context.getSystemService(LocaleManager::class.java).applicationLocales =
+                LocaleList.forLanguageTags(languageCode)
+        } else {
+            //version < 13
+            AppCompatDelegate.setApplicationLocales(LocaleListCompat.forLanguageTags(languageCode))
+            }
+        }
+
+    fun formatNumber(value: Int): String {
+        val formatter = NumberFormat.getInstance(Locale.getDefault())
+        return formatter.format(value)
+    }
+}

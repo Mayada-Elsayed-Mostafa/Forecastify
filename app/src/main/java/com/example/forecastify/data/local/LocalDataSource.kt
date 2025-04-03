288f198c481793edbf0f3ea56a6d4cd0127bbb43
Mayada-Elsayed-Mostafa <mayyadaelsayed8@gmail.com>
2025-04-03 23:59:02 +0200

    Extend LocalDataSource to manage weather, forecast, favorite locations, and alarms in local database.

--- app/src/main/java/com/example/forecastify/data/local/LocalDataSource.kt
+++ app/src/main/java/com/example/forecastify/data/local/LocalDataSource.kt
@@ -1,5 +1,30 @@
 package com.example.forecastify.data.local
 
+import com.example.forecastify.data.models.AlarmItem
+import com.example.forecastify.data.models.FavoriteLocation
+import com.example.forecastify.data.models.ForecastResponse
+import com.example.forecastify.data.models.WeatherResponse
+import kotlinx.coroutines.flow.Flow
+
 interface LocalDataSource {
 
+    suspend fun getWeatherFromRoom(): Flow<WeatherResponse>
+
+    suspend fun insertWeather(weatherResponse: WeatherResponse)
+
+    suspend fun getForecastFromRoom(): Flow<ForecastResponse>
+
+    suspend fun insertForecast(forecastResponse: ForecastResponse)
+
+    fun getAllLocations(): Flow<List<FavoriteLocation>>
+
+    suspend fun insertLocation(location: FavoriteLocation)
+
+    suspend fun deleteLocation(id: Int)
+
+    suspend fun getAllAlarms(): Flow<List<AlarmItem>>
+
+    suspend fun insertAlarm(alertItem: AlarmItem)
+
+    suspend fun deleteAlarm(alarmItem: AlarmItem)
 }

4d94842b599d0c8dd0904479bea6d8fda79f1a56
Mayada-Elsayed-Mostafa <mayyadaelsayed8@gmail.com>
2025-04-03 23:37:01 +0200

    Implement repository methods for managing alarms and data syncing with local and remote sources.

--- app/src/main/java/com/example/forecastify/data/repository/RepositoryImp.kt
+++ app/src/main/java/com/example/forecastify/data/repository/RepositoryImp.kt
@@ -1,24 +1,56 @@
 package com.example.forecastify.data.repository
 
 import com.example.forecastify.data.local.LocalDataSourceImp
+import com.example.forecastify.data.models.AlarmItem
 import com.example.forecastify.data.models.FavoriteLocation
 import com.example.forecastify.data.models.ForecastResponse
 import com.example.forecastify.data.models.WeatherResponse
 import com.example.forecastify.data.remote.RemoteDataSourceImp
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.flow.flowOf
 
 class RepositoryImp private constructor(
     private val remoteDataSource: RemoteDataSourceImp,
     private val localDataSource: LocalDataSourceImp,
 ) : Repository {
 
-    override suspend fun getWeather(isOnline: Boolean, lat: Double, lon: Double): WeatherResponse? {
-        return remoteDataSource.getCurrentWeather(lat, lon)
+    override suspend fun getWeather(
+        isOnline: Boolean,
+        lat: Double,
+        lon: Double,
+    ): Flow<WeatherResponse> {
+        return if (isOnline) {
+            val result = remoteDataSource.getCurrentWeather(lat, lon)
+            if (result != null) {
+                localDataSource.insertWeather(result)
+                flowOf(result)
+            } else {
+                emptyFlow()
+            }
+        } else {
+            localDataSource.getWeatherFromRoom()
+        }
     }
 
-    override suspend fun getUpcomingForecast(lat: Double, lon: Double): ForecastResponse? {
-        return remoteDataSource.getForecast(lat, lon)
+    override suspend fun getUpcomingForecast(
+        isOnline: Boolean,
+        lat: Double,
+        lon: Double,
+    ): Flow<ForecastResponse> {
+        return if (isOnline) {
+            val result = remoteDataSource.getForecast(lat, lon)
+            if (result != null) {
+                localDataSource.insertForecast(result)
+                flowOf(result)
+            } else {
+                emptyFlow()
+            }
+        } else {
+            localDataSource.getForecastFromRoom()
+        }
     }
+
 
     override fun getFavoriteLocations(): Flow<List<FavoriteLocation>> {
         return localDataSource.getAllLocations()
@@ -30,6 +62,22 @@ class RepositoryImp private constructor(
 
     override suspend fun deleteLocation(id: Int) {
         localDataSource.deleteLocation(id)
+    }
+
+    override suspend fun getAllAlarms(): Flow<List<AlarmItem>> {
+        return localDataSource.getAllAlarms()
+    }
+
+    override suspend fun addAlarm(alarmItem: AlarmItem) {
+        localDataSource.insertAlarm(alarmItem)
+    }
+
+    override suspend fun removeAlarm(alarmItem: AlarmItem) {
+        localDataSource.deleteAlarm(alarmItem)
+    }
+
+    override suspend fun deleteAlarmTime(time: String) {
+        localDataSource.deleteAlarmTime(time)
     }
 
     companion object {

ce5d976d5a5c940562bd9bea7bdf87b56eeac083
Mayada-Elsayed-Mostafa <mayyadaelsayed8@gmail.com>
2025-04-03 23:57:12 +0200

    Implement FavoriteDao for managing favorite locations in the local database.

--- app/src/main/java/com/example/forecastify/data/local/FavoriteDao.kt
+++ app/src/main/java/com/example/forecastify/data/local/FavoriteDao.kt
@@ -1,9 +1,21 @@
 package com.example.forecastify.data.local
 
-import androidx.room.*
-import com.example.forecastify.data.models.WeatherResponse
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.forecastify.data.models.FavoriteLocation
+import kotlinx.coroutines.flow.Flow
 
 @Dao
-interface WeatherDao {
+interface FavoriteDao {
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertLocation(location: FavoriteLocation)
+
+    @Query("SELECT * FROM favorite_location_table")
+    fun getAllLocations(): Flow<List<FavoriteLocation>>
+
+    @Query("DELETE FROM favorite_location_table WHERE id = :id")
+    suspend fun deleteLocation(id: Int)
 }

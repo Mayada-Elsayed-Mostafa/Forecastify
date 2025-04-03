72efbf84bdc333cdc7960d2c214e025defea75ec
Mayada-Elsayed-Mostafa <mayyadaelsayed8@gmail.com>
2025-04-03 23:41:40 +0200

    Refactor RemoteDataSourceImp to remove null handling and exceptions.

--- app/src/main/java/com/example/forecastify/data/remote/RemoteDataSourceImp.kt
+++ app/src/main/java/com/example/forecastify/data/remote/RemoteDataSourceImp.kt
@@ -2,37 +2,17 @@ package com.example.forecastify.data.remote
 
 import com.example.forecastify.data.models.ForecastResponse
 import com.example.forecastify.data.models.WeatherResponse
-import java.io.IOException
-import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.Locale
 
 class RemoteDataSourceImp(private val service: WeatherService) : RemoteDataSource {
 
     val apiKey = "65f5ae26a23ee25d8e12c2afeb2a72a3"
 
-    override suspend fun getCurrentWeather(lat: Double, lon: Double): WeatherResponse? {
-        return service.getCurrentWeather(lat, lon, apiKey).body()
+    override suspend fun getCurrentWeather(lat: Double, lon: Double): WeatherResponse {
+        return service.getCurrentWeather(lat, lon, apiKey).body()!!
     }
 
-    override suspend fun getForecastOfDay(lat: Double, lon: Double): ForecastResponse? {
-        return try {
-            val response = service.getForecastOfDay(lat, lon, apiKey)
-            if (response.isSuccessful) {
-                val forecastResponse = response.body()
-
-                val todayDate = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(Date())
-
-                val todayForecasts =
-                    forecastResponse?.list?.filter { it.dt_txt.startsWith(todayDate) }
-
-                forecastResponse?.copy(list = todayForecasts ?: emptyList())
-            } else {
-                null
-            }
-        } catch (e: IOException) {
-            null
-        }
+    override suspend fun getForecast(lat: Double, lon: Double): ForecastResponse {
+        return service.getForecast(lat, lon, apiKey).body()!!
     }
 
 }

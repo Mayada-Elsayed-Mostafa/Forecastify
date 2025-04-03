97a413ace50edcb909ec216f84ac388d90da4e50
Mayada-Elsayed-Mostafa <mayyadaelsayed8@gmail.com>
2025-04-03 23:55:06 +0200

    Refactor AppDatabase to support multiple entities including Alarms, Favorite Locations, Weather, and Forecasts.

--- app/src/main/java/com/example/forecastify/data/local/AppDatabase.kt
+++ app/src/main/java/com/example/forecastify/data/local/AppDatabase.kt
@@ -5,29 +5,33 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.example.forecastify.data.models.AlarmItem
+import com.example.forecastify.data.models.FavoriteLocation
+import com.example.forecastify.data.models.ForecastResponse
 import com.example.forecastify.data.models.WeatherResponse
 
 @Database(
-    entities = [WeatherResponse::class],
-    version = 1,
+    entities = [AlarmItem::class, FavoriteLocation::class, WeatherResponse::class, ForecastResponse::class],
+    version = 5,
     exportSchema = false
 )
 @TypeConverters(WeatherTypeConverter::class)
-abstract class WeatherDatabase : RoomDatabase() {
+abstract class AppDatabase : RoomDatabase() {
 
+    abstract fun getFavoriteDao(): FavoriteDao
+    abstract fun getAlarmDao(): AlarmDao
     abstract fun getWeatherDao(): WeatherDao
+    abstract fun getForecastDao(): ForecastDao
 
     companion object {
         @Volatile
-        private var INSTANCE: WeatherDatabase? = null
+        private var INSTANCE: AppDatabase? = null
 
-        fun getInstance(context: Context): WeatherDatabase {
+        fun getInstance(context: Context): AppDatabase {
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    WeatherDatabase::class.java,
-                    "weather_database"
-                ).build()
+                    context.applicationContext, AppDatabase::class.java, "All_database"
+                ).fallbackToDestructiveMigration().build()
                 INSTANCE = instance
                 instance
             }

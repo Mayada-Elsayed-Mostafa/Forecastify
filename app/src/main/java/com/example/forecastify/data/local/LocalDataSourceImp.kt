1355ba126025aaeb044f4969cfe6fe4aa346aadc
Mayada-Elsayed-Mostafa <mayyadaelsayed8@gmail.com>
2025-04-03 23:59:56 +0200

    Implement LocalDataSource with DAO methods for weather, forecast, favorite locations, and alarms

--- app/src/main/java/com/example/forecastify/data/local/LocalDataSourceImp.kt
+++ app/src/main/java/com/example/forecastify/data/local/LocalDataSourceImp.kt
@@ -1,7 +1,61 @@
 package com.example.forecastify.data.local
 
+import com.example.forecastify.data.models.AlarmItem
+import com.example.forecastify.data.models.FavoriteLocation
+import com.example.forecastify.data.models.ForecastResponse
+import com.example.forecastify.data.models.WeatherResponse
+import kotlinx.coroutines.flow.Flow
 
-class LocalDataSourceImp(private val weatherDao: com.example.forecastify.data.local.WeatherDao) :
-    com.example.forecastify.data.local.LocalDataSource {
+
+class LocalDataSourceImp(
+    private val favoriteDao: FavoriteDao,
+    private val weatherDao: WeatherDao,
+    private val forecastDao: ForecastDao,
+    private val alarmDao: AlarmDao,
+) : LocalDataSource {
+
+    override suspend fun getWeatherFromRoom(): Flow<WeatherResponse> {
+        return weatherDao.getWeather()
+    }
+
+    override suspend fun getForecastFromRoom(): Flow<ForecastResponse> {
+        return forecastDao.getForecast()
+    }
+
+    override suspend fun insertWeather(weatherResponse: WeatherResponse) {
+        weatherDao.insertWeather(weatherResponse)
+    }
+
+    override suspend fun insertForecast(forecastResponse: ForecastResponse) {
+        forecastDao.insertForecast(forecastResponse)
+    }
+
+    override fun getAllLocations(): Flow<List<FavoriteLocation>> {
+        return favoriteDao.getAllLocations()
+    }
+
+    override suspend fun insertLocation(location: FavoriteLocation) {
+        return favoriteDao.insertLocation(location)
+    }
+
+    override suspend fun deleteLocation(id: Int) {
+        return favoriteDao.deleteLocation(id)
+    }
+
+    override suspend fun getAllAlarms(): Flow<List<AlarmItem>> {
+        return alarmDao.getAllAlarms()
+    }
+
+    override suspend fun insertAlarm(alertItem: AlarmItem) {
+        alarmDao.insertAlarm(alertItem)
+    }
+
+    override suspend fun deleteAlarm(alarmItem: AlarmItem) {
+        alarmDao.deleteAlarm(alarmItem)
+    }
+
+    suspend fun deleteAlarmTime(time: String){
+        alarmDao.deleteAlarmTime(time)
+    }
 
 }
